d810f6de61995ab90cdeff0928a19275dbf46d25
paschalc24 <pcorrigan@umass.edu>
2023-04-19 16:47:25 -0400

    Removed react strict mode and styled header.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
